416c966847377841e25c782ab6b4197c6e5d9a5d
Abe_Hsiao <abe_hsiao@qware.com.tw>
2026-01-16 10:50:46 +0800

    feat : add fluent assert

--- CICDTest.Tests/CalculatorTest.cs
+++ CICDTest.Tests/CalculatorTest.cs
@@ -1,31 +1,25 @@
-﻿namespace CICDTest.Tests
+﻿using FluentAssertions;
+
+namespace CICDTest.Tests;
+
+public class CalculatorTest
 {
-    public class CalculatorTest(Calculator calculator)
+    private readonly Calculator _calculator = new();
+    [Fact]
+    public void Add_ReturnsCorrectSum()
     {
-        private readonly Calculator _calculator = calculator;
-        [Fact]
-        public void Add_ReturnsCorrectSum()
-        {
-            // Arrange
-            var calc = new Calculator();
-
-            // Act
-            int result = calc.Add(3, 5);
-
-            // Assert
-            Assert.Equal(8, result);
-        }
-
-        [Theory]
-        [InlineData(2, 3, 6)]
-        [InlineData(0, 5, 0)]
-        [InlineData(-2, 4, -8)]
-        public void Multiply_ReturnsCorrectProduct(int a, int b, int expected)
-        {
-            var calc = new Calculator();
-            int result = calc.Multiply(a, b);
-            Assert.Equal(expected, result);
-        }
-
+        var result = _calculator.Add(3, 5);
+        result.Should().Be(8);
     }
+
+    [Theory]
+    [InlineData(2, 3, 6)]
+    [InlineData(0, 5, 0)]
+    [InlineData(-2, 4, -8)]
+    public void Multiply_ReturnsCorrectProduct(int a, int b, int expected)
+    {
+        var result = _calculator.Multiply(a, b);
+        result.Should().Be(expected);
+    }
+
 }
