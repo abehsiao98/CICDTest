4d6efa4405a3b65bbb8cbb8935911357f51c4922
Abe_Hsiao <abe_hsiao@qware.com.tw>
2026-01-16 10:40:46 +0800

    feat : add test

---
+++ CICDTest.Tests/CalculatorTest.cs
@@ -0,0 +1,31 @@
+﻿namespace CICDTest.Tests
+{
+    public class CalculatorTest(Calculator calculator)
+    {
+        private readonly Calculator _calculator = calculator;
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            // Arrange
+            var calc = new Calculator();
+
+            // Act
+            int result = calc.Add(3, 5);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 6)]
+        [InlineData(0, 5, 0)]
+        [InlineData(-2, 4, -8)]
+        public void Multiply_ReturnsCorrectProduct(int a, int b, int expected)
+        {
+            var calc = new Calculator();
+            int result = calc.Multiply(a, b);
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
---
+++ CICDTest/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace CICDTest;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
